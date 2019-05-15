b81453afa7de95262b5d769cf9827289d620a5ae
Patryk Obara <dreamer.tan@gmail.com>
2019-05-15 18:55:21 +0200

    tweaks: Add tweak for Jagged Alliance Gold
    This game runs perfectly under Linux as-is, but comes with old DOSBox
    version.

--- tweaks.py
+++ tweaks.py
@@ -20,6 +20,25 @@ TWEAKS_DB = {
             },
         },
     },
+    # Jagged Alliance Gold
+    '283270': {
+        'commands': {
+            r'.*/Jagged Alliance/run.bat': {
+                'args': ['-c', 'mount C Installed',
+                         '-c', 'imgmount D cd.iso -t cdrom',
+                         '-c', 'C:',
+                         '-c', 'cd jagged',
+                         '-c', 'ja.bat'],
+            },
+            r'.*/Jagged Alliance Deadly Games/run.bat': {
+                'args': ['-c', 'mount C Installed',
+                         '-c', 'imgmount D cd.iso -t cdrom',
+                         '-c', 'C:',
+                         '-c', 'cd deadly',
+                         '-c', 'deadly.bat'],
+            },
+        },
+    },
     # MegaRace 2
     '733760': {
         'commands': {
@@ -46,5 +65,5 @@ def tweak_command(app_id, cmd_line):
             if 'args' in replacement:
                 return replacement['args']
             raise KeyError
-    print_err('run_dosbox: warning: no suitable command tweak found')
+    print_err('run_dosbox: error: no suitable tweak found for:', cmd_line)
     return []
