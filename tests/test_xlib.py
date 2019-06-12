559a0c280d9e576ee6f3d6f40ddc6132c468a0f0
Patryk Obara <dreamer.tan@gmail.com>
2019-06-12 12:39:10 +0200

    xlib: Add unit test

---
+++ tests/test_xlib.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# pylint: disable=missing-docstring
+# pylint: disable=wrong-spelling-in-comment
+
+import unittest
+
+import xlib
+
+
+class TestXlib(unittest.TestCase):
+
+    def test_no_exception(self):
+        # There's not much we can test here without conditionally
+        # disabling tests on CI…
+        screens = xlib.query_screens()
+        self.assertEqual(screens.__class__, dict)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
